3db9984767965b723cc7235991bed9dff19e8dad
car <marvinlim4th@gmail.com>
2025-06-19 02:14:06 -0400

    fixing jetson spi

--- jetson/spi.cpp
+++ jetson/spi.cpp
@@ -4,11 +4,13 @@
 #include <sys/ioctl.h>
 #include <linux/spi/spidev.h>
 #include <cstring>
+#include <chrono>
+#include <thread>
 
 int main() {
     uint8_t mode = SPI_MODE_0;
     uint8_t bits = 8;
-    uint32_t speed = 500000; // 500 kHz
+    uint32_t speed = 100000; // 500 kHz
 
     int fd = open("/dev/spidev0.0", O_RDWR);
     if (fd < 0) {
@@ -37,27 +39,31 @@ int main() {
         return 1;
     }
 
-    uint8_t tx[] = { 0x9F }; // Transmit Buffer
+    uint8_t tx[] = { 0x9F, 0x00, 0x00, 0x00, 0x9F, 0x00, 0x00, 0x00 }; // Transmit Buffer
     uint8_t rx[sizeof(tx)] = {0}; // Receive Buffer
 
-    struct spi_ioc_transfer tr = {
-        .tx_buf = (unsigned long)tx,
-        .rx_buf = (unsigned long)rx,
-        .len = sizeof(tx),
-        .speed_hz = speed,
-        .bits_per_word = bits
-    };
+    struct spi_ioc_transfer tr = {0};
+    tr.tx_buf = (unsigned long)tx;
+    tr.rx_buf = (unsigned long)rx;
+    tr.len = sizeof(tx);
+    tr.speed_hz = speed;
+    tr.bits_per_word = bits;
+    tr.delay_usecs = 1000;
 
-    // Perform SPI Transaction
-    if (ioctl(fd, SPI_IOC_MESSAGE(1), &tr) < 0) {
-        std::cout << "SPI transfer failed" << std::endl;
-        close(fd);
-        return 1;
-    }
+    while (1) {
+        // Perform SPI Transaction
+        if (ioctl(fd, SPI_IOC_MESSAGE(1), &tr) < 0) {
+            std::cout << "SPI transfer failed" << std::endl;
+            close(fd);
+            return 1;
+        }
 
-    std::cout << "Received: ";
-    for (size_t i = 0; i < sizeof(rx); i++) {
-        printf("0x%02X", rx[i]);
+        std::cout << "Received: ";
+        for (size_t i = 0; i < sizeof(rx); i++) {
+            printf("0x%02X\n", rx[i]);
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     }
 
 }
